bbf499717dbcae049de5d7eee45ce9f96dfb5373
Zeeshan Malik <zeeshan.malik@rci.rogers.com>
2021-09-16 01:18:19 -0400

    added one test

--- test/test_job_one.py
+++ test/test_job_one.py
@@ -14,11 +14,9 @@ class joboneTest(unittest.TestCase):
         self.spark = self.con.spark
         self.sparkContext = self.con.sc
 
-    def test_filteration(self):
+    def schema_creation(self):
 
-        data = [('q6542454412614060','50:95:51:D4:7E:13',['null'],'Start','1622841283259','STB-XI6','null','null','null',
-                 ['null'],'null','null','null','null','null','null','null',['null'],'null','null')]
-        schema = StructType([
+        return StructType([
             StructField('accountSourceId', StringType(), True),
             StructField('deviceSourceId', StringType(), True),
             StructField('bitrate', ArrayType(StringType()), True),
@@ -27,7 +25,7 @@ class joboneTest(unittest.TestCase):
             StructField('applicationname', StringType(), True),
             StructField('completionstatus', StringType(), True),
             StructField('assetclass', StringType(), True),
-            StructField('sessionduration', StringType(), True),
+            StructField('sessionduration', IntegerType(), True),
             StructField('playstateChanged', ArrayType(StringType()), True),
             StructField('pluginSessionId', StringType(), True),
             StructField('playbackId', StringType(), True),
@@ -41,6 +39,11 @@ class joboneTest(unittest.TestCase):
             StructField('az_insert_ts', StringType(), True)
         ])
 
+    def test_filteration_one(self):
+
+        data = [('q6542454412614060','50:95:51:D4:7E:13',['null'],'Start','1622841283259','STB-XI6','null','null',0,
+                 ['null'],'null','null','null','null','null','null','null',['null'],'null','null')]
+        schema = self.schema_creation()
 
         empty_schema = StructType([
             StructField('accountSourceId', StringType(), True),
@@ -69,9 +72,48 @@ class joboneTest(unittest.TestCase):
 
         self.assertEquals (job_1.__filteration__(df).collect(),empty_df.collect())
 
+    def test_filteration_two(self):
+
+        data = [('D6604896516169469','18:9C:27:49:BD:0D', ['1'], 'End', '1622841283259', 'STB-XI6', '1', 'Linear',
+                 300001, ['initializing'], '1', '1', '1', '1', '1', '1', '1', ['1622841283269'], '1', '1')]
+
+        schema = self.schema_creation()
+        rdd = self.sparkContext.parallelize(data)
+
+        df = self.spark.createDataFrame(rdd,schema)
+
+        job_1 = parsing()
+
+        filled_schema = StructType([
+            StructField('accountSourceId', StringType(), True),
+            StructField('deviceSourceId', StringType(), True),
+            StructField('starttime', StringType(), True),
+            StructField('sessionduration', IntegerType(), True),
+            StructField('gracenoteId', StringType(), True),
+            StructField('bitrate', ArrayType(StringType()), True),
+            StructField('pluginSessionId', StringType(), True),
+            StructField('playbackId', StringType(), True),
+            StructField('ff_shifts_present', IntegerType(), True),
+            StructField('clientGeneratedTimestamp_flattened', StringType(), True),
+            StructField('clientGeneratedTimestamp', ArrayType(StringType()), True),
+            StructField('bitrate_flattened', StringType(), True),
+            StructField('az_insert_ts', StringType(), True)])
+
+        data_f = [('D6604896516169469','18:9C:27:49:BD:0D', '1622841283259', 300001, None, ['1'], '1', '1',
+                 0,'1622841283269',['1622841283269'], '1','1')]
+
+        rdd2 = self.spark.sparkContext.parallelize(data_f)
+
+        df1 = self.spark.createDataFrame(rdd2, filled_schema)
+
+        self.assertEquals(job_1.__filteration__(df).collect(), df1.collect())
+
+
 if __name__ == '__main__':
     test_1 = joboneTest()
-    test_1.test_filteration()
+    test_1.test_filteration_one()
+    test_1.test_filteration_two()
+
 
 
 
